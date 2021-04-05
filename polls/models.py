bdef5ed7c9d13d46b57ba45345cafb45cfebdd0e
Tom Bauer <tbauer1898@gmail.com>
2021-04-05 18:40:52 -0500

    first commit

---
+++ polls/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+# question has question and publication date
+# a choice has two fields : the text of the choice and a vote tally. Each choice is associated with a question 
