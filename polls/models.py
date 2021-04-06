2661be129fd3aba6577dde02e540edaf2005d11c
Tom Bauer <tbauer1898@gmail.com>
2021-04-06 11:59:18 -0500

    basic question/ choice models built

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,30 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 # question has question and publication date
 # a choice has two fields : the text of the choice and a vote tally. Each choice is associated with a question 
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+# this small bit of model code gives Django the means to do several things
+    # create a db schema - "Create table" statements 
+    # create a python database-access API for accessing Question and Choice objects
+
+#django apps are pluggable - you can use an app in multiple projects. Modular.
--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello senor. You have arrived at polls')
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+    
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+    
